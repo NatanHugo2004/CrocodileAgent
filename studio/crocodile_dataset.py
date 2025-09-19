e49e003912867c0b5832f5624f4227f6a15d4e98
NatanHugo2004 <natan.hugo.carvalho.evangelista@ccc.ufcg.edu.br>
2025-09-18 22:55:38 -0300

    feat: human input

---
+++ studio/crocodile_dataset.py
@@ -0,0 +1,11 @@
+import kagglehub
+import random
+import pandas as pd
+path = kagglehub.dataset_download("zadafiyabhrami/global-crocodile-species-dataset") + '/crocodile_dataset.csv'
+pd = pd.read_csv(path)
+
+lista_aleatoria = random.sample(range(0, len(pd)), 3)
+common_names = list(pd['Common Name'])
+common_names_dict = {pd.loc[i, "Common Name"]: pd.loc[i,"Notes"] for i in lista_aleatoria}
+crocodile_test = common_names[lista_aleatoria[0]]
+notes = common_names_dict["Siamese Crocodile"] 
